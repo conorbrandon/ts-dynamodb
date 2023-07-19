000394f1dbd271cdd4a60c16eb09e18bba951a01
Conor Brandon <conorsbrandon@gmail.com>
2023-07-19 11:34:57 -0400

    v1.2.2 - more nuanced check for missing Blob interface, requiring it to not only be defined but have actual properties matching Blob

--- src/dynamodb-types.ts
+++ src/dynamodb-types.ts
@@ -1,5 +1,4 @@
 import { DynamoDB } from "aws-sdk";
-import { IsAny } from "./type-helpers/utils";
 import { Stream } from "stream";
 
 export type ExpressionAttributeNames<Names extends string> = Record<`#${string}`, Names>;
@@ -9,7 +8,7 @@ export type ExpressionAttributeValues = Record<`:${string}`, any>;
 
 export type DynamoDBKeyValue = string | number | DynamoDB.DocumentClient.BinaryAttributeValue;
 
-type BlobTypePolyfill = IsAny<Blob> extends true ? never : Blob;
+type BlobTypePolyfill = Blob extends { readonly size: number; readonly type: string } ? Blob : never;
 // unfortunately there are these two stupid interfaces in the Documentclient.binaryType: File and Blob, which are literally just {}, which means a whole bunch of stuff extends the empty object (boolean, ANY object, and probably more). sigh.
 export type NativeJSBinaryTypes = Buffer | BlobTypePolyfill | ArrayBuffer | DataView | Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | Stream;
 
